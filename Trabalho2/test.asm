94b85741795f3c3d70b2d5b4efe7cfd99b641858
João Pedro <jotape.ufc@gmail.com>
2019-06-02 18:13:48 -0300

    quebra matriz em varias linhas e adicionado metodo de copiar chave

--- Trabalho2/test.asm
+++ Trabalho2/test.asm
@@ -1,19 +1,62 @@
 section .data
     msg db "msg.txt", 0         ;arquivo de com a mensagem que sera encriptada
-    len equ 1024
+    len equ $-msg
     key db "chave.txt", 0       ;arquivo com a chave de encriptacao e decriptacao
-    lenk equ 1024
-    matriz db 'abcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyzacdefghijklmnopqrstuvwxyzabdefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcdfghijklmnopqrstuvwxyzabcdeghijklmnopqrstuvwxyzabcdefhijklmnopqrstuvwxyzabcdefgijklmnopqrstuvwxyzabcdefghjklmnopqrstuvwxyzabcdefghiklmnopqrstuvwxyzabcdefghijlmnopqrstuvwxyzabcdefghijkmnopqrstuvwxyzabcdefghijklnopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmnpqrstuvwxyzabcdefghijklmnoqrstuvwxyzabcdefghijklmnoprstuvwxyzabcdefghijklmnopqstuvwxyzabcdefghijklmnopqrtuvwxyzabcdefghijklmnopqrsuvwxyzabcdefghijklmnopqrstvwxyzabcdefghijklmnopqrstuwxyzabcdefghijklmnopqrstuvxyzabcdefghijklmnopqrstuvwyzabcdefghijklmnopqrstuvwxzabcdefghijklmnopqrstuvwxy', 0
+    lenk equ $-key
+    matriz db "abcdefghijklmnopqrstuvwxyz",
+           db "bcdefghijklmnopqrstuvwxyza",
+           db "cdefghijklmnopqrstuvwxyzab",
+           db "defghijklmnopqrstuvwxyzabc",
+           db "efghijklmnopqrstuvwxyzabcd",
+           db "fghijklmnopqrstuvwxyzabcde",
+           db "ghijklmnopqrstuvwxyzabcdef",
+           db "hijklmnopqrstuvwxyzabcdefg",
+           db "ijklmnopqrstuvwxyzabcdefgh",
+           db "jklmnopqrstuvwxyzabcdefghi",
+           db "klmnopqrstuvwxyzabcdefghij",
+           db "lmnopqrstuvwxyzabcdefghijk",
+           db "mnopqrstuvwxyzabcdefghijkl",
+           db "nopqrstuvwxyzabcdefghijklm",
+           db "opqrstuvwxyzabcdefghijklmn",
+           db "pqrstuvwxyzabcdefghijklmno",
+           db "qrstuvwxyzabcdefghijklmnop",
+           db "rstuvwxyzabcdefghijklmnopq",
+           db "stuvwxyzabcdefghijklmnopqr",
+           db "tuvwxyzabcdefghijklmnopqrs",
+           db "uvwxyzabcdefghijklmnopqrst",
+           db "vwxyzabcdefghijklmnopqrstu",
+           db "wxyzabcdefghijklmnopqrstuv",
+           db "xyzabcdefghijklmnopqrstuvw",
+           db "yzabcdefghijklmnopqrstuvwx",
+           db "zabcdefghijklmnopqrstuvwxy", 0
     tam equ $-matriz
 
 section .bss 
-    buffer: resb 1024
+    buffer: resb 1024 
     kbuffer: resb 1024
+    keyExtend: resb 1024
 
 section .text
 global _start
 
 _start:
+    ;printar array
+    mov esi, matriz
+    mov edi, tam-1
+    printArray:                ;show array
+    mov edx, 1
+    mov ecx, [esi]
+    ;add ecx, 48                ;para converter pra ascii
+    push ecx
+    mov ecx, esp
+    mov ebx, 1
+    mov eax, 4
+    int 80h
+    pop ecx
+    inc esi
+    dec edi
+    jns printArray
+
     openMSG:                    ;open file for reading
     mov ebx, msg                ; name of the msg  
     mov eax, 5  
@@ -34,7 +77,6 @@ _start:
     mov ecx, 0  
     int 80h  
 
-
     readKEY:                    ;read from file
     mov eax, 3  
     mov ebx, eax
@@ -42,13 +84,24 @@ _start:
     mov edx, lenk    
     int 80h
     call close
-
+    
+    mov esi, 0                  ;percorrer o array da chave
+    mov edx, lenk               ;tamanho da chave
+    mov ecx, len                ;tamanho da mensagem
+    copy:
+    mov edi, [kbuffer+esi]      ;move para edi caracter por caracter
+    mov [keyExtend+esi], edi    ;move para key extend o caracter pego em cada iteração
+    cmp ecx, 0
+    je displayBuffer
+    dec ecx
+    inc esi
+    jmp copy
 
     displayBuffer:              ;write the message indicating end of file
     mov eax, 4  
     mov ebx, 1
-    mov ecx, buffer
-    mov edx, len    
+    mov ecx, keyExtend
+    mov edx, lenk
     int 80h
     call exit
 
@@ -56,23 +109,6 @@ _start:
     mov eax, 6  
     int 80h
     ret
-
-    ;printar array
-    ;mov esi, matriz
-    ;mov edi, tam-1
-    ;printArray:                ;show array
-    ;mov edx, 1
-    ;mov ecx, [esi]
-    ;add ecx, 48                ;para converter pra ascii
-    ;push ecx
-    ;mov ecx, esp
-    ;mov ebx, 1
-    ;mov eax, 4
-    ;int 0x80
-    ;pop ecx
-    ;inc esi
-    ;dec edi
-    ;jns printArray
 
     exit:                       ;exit program
     mov eax, 1  
