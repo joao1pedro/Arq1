5666169c02e1a3fbb5831fa1695a852b21e04144
João Pedro <jotape.ufc@gmail.com>
2019-06-02 18:19:42 -0300

    comentario linhas da matriz

--- Trabalho2/test.asm
+++ Trabalho2/test.asm
@@ -3,32 +3,32 @@ section .data
     len equ $-msg
     key db "chave.txt", 0       ;arquivo com a chave de encriptacao e decriptacao
     lenk equ $-key
-    matriz db "abcdefghijklmnopqrstuvwxyz",
-           db "bcdefghijklmnopqrstuvwxyza",
-           db "cdefghijklmnopqrstuvwxyzab",
-           db "defghijklmnopqrstuvwxyzabc",
-           db "efghijklmnopqrstuvwxyzabcd",
-           db "fghijklmnopqrstuvwxyzabcde",
-           db "ghijklmnopqrstuvwxyzabcdef",
-           db "hijklmnopqrstuvwxyzabcdefg",
-           db "ijklmnopqrstuvwxyzabcdefgh",
-           db "jklmnopqrstuvwxyzabcdefghi",
-           db "klmnopqrstuvwxyzabcdefghij",
-           db "lmnopqrstuvwxyzabcdefghijk",
-           db "mnopqrstuvwxyzabcdefghijkl",
-           db "nopqrstuvwxyzabcdefghijklm",
-           db "opqrstuvwxyzabcdefghijklmn",
-           db "pqrstuvwxyzabcdefghijklmno",
-           db "qrstuvwxyzabcdefghijklmnop",
-           db "rstuvwxyzabcdefghijklmnopq",
-           db "stuvwxyzabcdefghijklmnopqr",
-           db "tuvwxyzabcdefghijklmnopqrs",
-           db "uvwxyzabcdefghijklmnopqrst",
-           db "vwxyzabcdefghijklmnopqrstu",
-           db "wxyzabcdefghijklmnopqrstuv",
-           db "xyzabcdefghijklmnopqrstuvw",
-           db "yzabcdefghijklmnopqrstuvwx",
-           db "zabcdefghijklmnopqrstuvwxy", 0
+    matriz db "abcdefghijklmnopqrstuvwxyz",     ;linha1
+           db "bcdefghijklmnopqrstuvwxyza",     ;linha2
+           db "cdefghijklmnopqrstuvwxyzab",     ;linha3
+           db "defghijklmnopqrstuvwxyzabc",     ;linha4
+           db "efghijklmnopqrstuvwxyzabcd",     ;linha5
+           db "fghijklmnopqrstuvwxyzabcde",     ;linha6
+           db "ghijklmnopqrstuvwxyzabcdef",     ;linha7
+           db "hijklmnopqrstuvwxyzabcdefg",     ;linha8
+           db "ijklmnopqrstuvwxyzabcdefgh",     ;linha9
+           db "jklmnopqrstuvwxyzabcdefghi",     ;linha10
+           db "klmnopqrstuvwxyzabcdefghij",     ;linha11
+           db "lmnopqrstuvwxyzabcdefghijk",     ;linha12
+           db "mnopqrstuvwxyzabcdefghijkl",     ;linha13
+           db "nopqrstuvwxyzabcdefghijklm",     ;linha14
+           db "opqrstuvwxyzabcdefghijklmn",     ;linha15
+           db "pqrstuvwxyzabcdefghijklmno",     ;linha16
+           db "qrstuvwxyzabcdefghijklmnop",     ;linha17
+           db "rstuvwxyzabcdefghijklmnopq",     ;linha18
+           db "stuvwxyzabcdefghijklmnopqr",     ;linha19
+           db "tuvwxyzabcdefghijklmnopqrs",     ;linha20
+           db "uvwxyzabcdefghijklmnopqrst",     ;linha21
+           db "vwxyzabcdefghijklmnopqrstu",     ;linha22
+           db "wxyzabcdefghijklmnopqrstuv",     ;linha23
+           db "xyzabcdefghijklmnopqrstuvw",     ;linha24
+           db "yzabcdefghijklmnopqrstuvwx",     ;linha25
+           db "zabcdefghijklmnopqrstuvwxy", 0   ;linha26
     tam equ $-matriz
 
 section .bss 
